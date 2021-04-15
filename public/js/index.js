08a7e561c0f99f43919cf66aca92c71a536a2dac
Danya <danya.culakov@yandex.ru>
2021-04-15 10:36:36 +0300

    Rel 1 form 50%

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+document.onscroll = function() {myFunction()};
+
+let header = document.getElementById("myHeader");
+let sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
