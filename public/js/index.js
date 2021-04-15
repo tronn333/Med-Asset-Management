5228a4291e4c508d82b1be9e6ac0c104957eccb0
Hanna <testi.hanna@gmail.com>
2021-04-15 20:42:24 +0300

    actions

--- public/js/index.js
+++ public/js/index.js
@@ -14,9 +14,22 @@ function myFunction() {
 }
 
 const form = document.querySelector("#contactform");
+const anestasiaAr = document.getElementsByName("anestasia");
+const anestasia = anestasiaAr[0];
+const hranestesia = document.getElementById("anestesia1");
+const divanestesia = document.getElementById("anestesia")
+const subpodryadAr = document.getElementsByName("subpodryad");
+const subpodryad = subpodryadAr[0];
+const subpodryadDiv1 = document.getElementById("subpodryad1")
+const subpodryadHr1 = document.getElementById("subpodryad2")
+const subpodryadDiv2 = document.getElementById("subpodryad3")
+const subpodryadHr2 = document.getElementById("subpodryad4")
+
+
+
 
 form.addEventListener("click", (e) => {
-  console.log(e);
+  //console.log(e);
   if (e.target.matches("#stringAdd")) {
     e.preventDefault();
     let counter = 1;
@@ -35,3 +48,21 @@ form.addEventListener("click", (e) => {
     firstDiv.after(clone)
   }
 });
+
+
+anestasia.addEventListener("change", () => {
+  if (anestasia.value === "1") {
+    hranestesia.style = "display"
+    divanestesia.style = "display"
+  }
+}, false)
+
+subpodryad.addEventListener("change", () => {
+  if (subpodryad.value === "1") {
+    subpodryadDiv1.style = "display"
+    subpodryadHr1.style = "display"
+    subpodryadDiv2.style = "display"
+    subpodryadHr2.style = "display"
+  }
+}, false)
+
