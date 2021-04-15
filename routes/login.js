b9111f509b17b04b107821048a4e5bca0a7de8b6
Andrew <tronn333@gmail.com>
2021-04-15 14:49:16 +0300

    added logic to homepage buttons

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
+// const { Router } = require("express");
 const entry = require("../models/Entrie");
 const userModel = require("../models/user");
 
 router.get("/", (req, res) => {
-  res.redirect("/login");
+  res.redirect("/homepage");
 });
 
 
@@ -14,6 +15,11 @@ router.get('/logout', (req, res) => {
     return res.redirect('/')
   })
 });
+router.get('/yourforms',async (req,res) =>{
+  const entries = await entry.find({currentdepartment:`${req.session.user.department}`})
+  console.log(entries)
+  res.render('allforms',{entries})
+})
 
 router.get("/allforms",async (req,res) =>{
   const entries = await entry.find()
@@ -21,7 +27,7 @@ router.get("/allforms",async (req,res) =>{
   res.render('allforms',{entries})
 })
 router.get("/newform",async (req,res)=>{
-const newEntry = new entry({status:'editing',initiator:req.session.user.id,currentdepartment:'department'})
+const newEntry = new entry({status:'editing',initiator:req.session.user.id,currentdepartment:`${req.session.user.department}`})
 console.log(newEntry._id);
 await entry.create(newEntry)
 res.redirect(`entry/${newEntry._id}`)
