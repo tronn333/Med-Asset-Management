6600fbd163377f433824932686ca71abf0334f3d
Danya <danya.culakov@yandex.ru>
2021-04-16 15:24:00 +0300

    Rel14 css done

--- public/js/index.js
+++ public/js/index.js
@@ -17,16 +17,13 @@ const form = document.querySelector("#contactform");
 const anestasiaAr = document.getElementsByName("anestasia");
 const anestasia = anestasiaAr[0];
 const hranestesia = document.getElementById("anestesia1");
-const divanestesia = document.getElementById("anestesia")
+const divanestesia = document.getElementById("anestesia");
 const subpodryadAr = document.getElementsByName("subpodryad");
 const subpodryad = subpodryadAr[0];
-const subpodryadDiv1 = document.getElementById("subpodryad1")
-const subpodryadHr1 = document.getElementById("subpodryad2")
-const subpodryadDiv2 = document.getElementById("subpodryad3")
-const subpodryadHr2 = document.getElementById("subpodryad4")
-
-
-
+const subpodryadDiv1 = document.getElementById("subpodryad1");
+const subpodryadHr1 = document.getElementById("subpodryad2");
+const subpodryadDiv2 = document.getElementById("subpodryad3");
+const subpodryadHr2 = document.getElementById("subpodryad4");
 
 form.addEventListener("click", (e) => {
   //console.log(e);
@@ -35,34 +32,40 @@ form.addEventListener("click", (e) => {
     let counter = 1;
     let div = e.target.closest("div");
     console.log(div);
-    let firstDiv = div.firstElementChild
+    let firstDiv = div.firstElementChild;
     let clone = firstDiv.cloneNode(true);
     let arr = [...clone.children];
     for (let i = 0; i < arr.length; i++) {
-      let theName = arr[i].getAttribute('name');
-      arr[i].value = " "
-      arr[i].setAttribute('name', theName + counter);
+      let theName = arr[i].getAttribute("name");
+      arr[i].value = "";
+      arr[i].setAttribute("name", theName + counter);
     }
     counter += 1;
     console.log(clone);
-    firstDiv.after(clone)
+    firstDiv.after(clone);
   }
 });
 
+anestasia.addEventListener(
+  "change",
+  () => {
+    if (anestasia.value === "1") {
+      hranestesia.style = "display";
+      divanestesia.style = "display";
+    }
+  },
+  false
+);
 
-anestasia.addEventListener("change", () => {
-  if (anestasia.value === "1") {
-    hranestesia.style = "display"
-    divanestesia.style = "display"
-  }
-}, false)
-
-subpodryad.addEventListener("change", () => {
-  if (subpodryad.value === "1") {
-    subpodryadDiv1.style = "display"
-    subpodryadHr1.style = "display"
-    subpodryadDiv2.style = "display"
-    subpodryadHr2.style = "display"
-  }
-}, false)
-
+subpodryad.addEventListener(
+  "change",
+  () => {
+    if (subpodryad.value === "1") {
+      subpodryadDiv1.style = "display";
+      subpodryadHr1.style = "display";
+      subpodryadDiv2.style = "display";
+      subpodryadHr2.style = "display";
+    }
+  },
+  false
+);
